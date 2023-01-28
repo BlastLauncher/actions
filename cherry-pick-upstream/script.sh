1c99b172631c0effa7e165ca1ca920cae526e172
Yukai Huang <yukaihuangtw@gmail.com>
2023-01-28 17:30:49 +0800

    fix: preserve original commit date

--- cherry-pick-upstream/script.sh
+++ cherry-pick-upstream/script.sh
@@ -9,5 +9,10 @@ start_sha1=$(git log --pretty=format:"%h" --first-parent upstream/main --since=$
 # Get upstream/main branch HEAD commit SHA1
 upstream_main_head_sha1=$(git rev-parse upstream/main)
 
-# Cherry-pick all commits on upstream/main
-git cherry-pick $start_sha1..$upstream_main_head_sha1
+# Cherry-pick all commits on upstream/main while preserving original commit date
+while read sha1; do
+    commit_date=$(git log -1 --pretty=format:"%ct" $sha1)
+    export GIT_AUTHOR_DATE=$commit_date
+    export GIT_COMMITTER_DATE=$commit_date
+    git cherry-pick $sha1
+done < <(git rev-list --reverse $start_sha1..$upstream_main_head_sha1)
